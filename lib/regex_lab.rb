77291f1559a52c915f6313e698f56298ec8b62df
Richard Lee <richlee81192@gmail.com>
2019-09-15 20:16:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,13 @@
 def starts_with_a_vowel?(word)
-if word.scan(/\A+[aeiou]/)
+if word.grep(/\A+[aeiou]/)
   true
+else
+  false 
 end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan
 end
 
 def words_five_letters_long(text)
